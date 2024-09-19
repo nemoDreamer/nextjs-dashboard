ad2c7e497a91276b89652afa72ccd42da9966fac
Philip Blyth <nemoDreamer@users.noreply.github.com>
2024-09-19 14:11:06 -0400

    Stream dashboard

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -13,13 +13,12 @@ export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
-
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log("Fetching revenue data...");
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log("Data fetch completed after 3 seconds.");
 
     return data.rows;
   } catch (error) {
@@ -64,6 +63,7 @@ export async function fetchCardData() {
       invoiceCountPromise,
       customerCountPromise,
       invoiceStatusPromise,
+      new Promise((resolve) => setTimeout(resolve, 1500)),
     ]);
 
     const numberOfInvoices = Number(data[0].rows[0].count ?? "0");
